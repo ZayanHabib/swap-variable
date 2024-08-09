ef74180bd5cd7121cd7646462ac8538673ae8ad8
Zayan Habib <zayanhabib37@gmail.com>
2024-08-09 14:10:54 +0500

    Add files via upload

---
+++ swap-variable.py
@@ -0,0 +1,37 @@
+# Swap Value Using Third Variable By Zayan
+
+print("Swap Value Using Third Variable")
+
+num1 = int(input("Enter A Number\n:"))
+num2 = int(input("Enter Another Number\n:"))
+
+print(f"num1 : {num1}")
+print(f"num2 : {num2}")
+
+swap = num2
+num2 = num1
+num1 = swap
+
+print(f"num1 : {num1}")
+print(f"num2 : {num2}")
+
+
+# Swap Value Using Third Variable By Zayan
+
+# Swap Value Without Using Third Variable By Zayan
+
+print("Swap Value Without Using Third Variable")
+
+num1 = int(input("Enter A Number\n:"))
+num2 = int(input("Enter Another Number\n:"))
+
+print(f"num1 : {num1}")
+print(f"num2 : {num2}")
+
+num1 = num1 + num2
+num2 = num1 - num2
+num1 = num1 - num2
+
+print(f"num1 : {num1}")
+print(f"num2 : {num2}")
+# Swap Value Without Using Third Variable By Zayan
